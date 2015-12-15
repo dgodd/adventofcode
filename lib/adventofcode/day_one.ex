39abc5cfcf5d36671e4cf19e00d135f09b1305ef
Dave Goddard <dgoddard@pivotal.io>
2015-12-14 22:51:23 -0500

    Day One part 2

--- lib/adventofcode/day_one.ex
+++ lib/adventofcode/day_one.ex
@@ -14,4 +14,26 @@ defmodule Adventofcode.DayOne do
   def finalfloor(")" <> str) do
     -1 + finalfloor(str)
   end
+
+  def _basementat(start, "") do
+    case finalfloor(start) do
+      -1 -> 1
+    end
+  end
+
+  def _basementat(start, finito) do
+    if finalfloor(start) == -1 do
+      1
+    else
+      base = byte_size(")")
+      <<x :: binary-size(base), rest :: binary>> = finito
+      1 + _basementat(start <> x, rest)
+    end
+  end
+
+  def basementat(full) do
+    base = byte_size(")")
+    <<x :: binary-size(base), rest :: binary>> = full
+    _basementat(x, rest)
+  end
 end
--- test/day_one_test.exs
+++ test/day_one_test.exs
@@ -27,8 +27,21 @@ defmodule Adventofcode.DayOneTest do
     assert Adventofcode.DayOne.finalfloor(")())())") == -3
   end
 
-  test "input file" do
+  test "input file for finalfloor" do
     {:ok, str} = File.read "test/day_one.txt"
     assert Adventofcode.DayOne.finalfloor(str) == 232
+  end
+
+  test ") causes him to enter the basement at character position 1." do
+    assert Adventofcode.DayOne.basementat(")") == 1
+  end
+
+  test "()()) causes him to enter the basement at character position 5" do
+    assert Adventofcode.DayOne.basementat("()())") == 5
+  end
+
+  test "input file for basementat" do
+    {:ok, str} = File.read "test/day_one.txt"
+    assert Adventofcode.DayOne.basementat(str) == 1783
   end
 end
