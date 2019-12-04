f55776276972752f5863cca3d86ab95b26d04402
Dave Goddard <dave@goddard.id.au>
2019-12-04 20:45:06 +1100

    2019 - Day 4 - REFACTOR
    Pro: Easy code to read, eg: using #each_cons
    Con: Half as fast as before

--- 2019/q4/password_test.rb
+++ 2019/q4/password_test.rb
@@ -1,24 +1,25 @@
 require 'minitest/autorun'
 require 'minitest/spec'
 
-def valid?(num)
-  double = false
-  digit = nil
-  # Go backwards for digits, so make sure they get smaller
-  loop do
-    prev_digit = digit
-    digit = num % 10
-    num = num / 10 
-
-    return false if prev_digit && prev_digit < digit
-    double = true if prev_digit == digit
-
-    return double if num == 0
+def array2(num)
+  arr = []
+  while num > 0 do
+    arr << num % 10
+    num = num / 10
   end
+  arr
 end
 
-def two_adjacent_matching?(num)
-  num.to_s.split('').chunk_while { |i, j| i == j }.map(&:count).any?(2)
+def valid?(num)
+  arr = array2(num).each_cons(2)
+  # Backwards, so make sure diminishing
+  arr.all? { |i, j| i >= j } && arr.any? { |i, j| i == j }
+end
+
+def valid2?(num)
+  arr = array2(num)
+  # Backwards, so make sure diminishing
+  arr.each_cons(2).all? { |i, j| i >= j } && arr.chunk_while { |i, j| i == j }.any? { |a| a.length == 2 }
 end
 
 describe 'password' do
@@ -30,14 +31,14 @@ describe 'password' do
     it { assert !valid?(123789) }
   end
 
-  describe '#only_two_adjacent_matching?' do
-    it { assert two_adjacent_matching?(122345) }
-    it { assert !two_adjacent_matching?(111123) }
-    it { assert !two_adjacent_matching?(111111) }
-    it { assert two_adjacent_matching?(223450) }
-    it { assert !two_adjacent_matching?(123789) }
-    it { assert !two_adjacent_matching?(123444) }
-    it { assert two_adjacent_matching?(111122) }
+  describe '#only_valid2?' do
+    it { assert valid2?(122345) }
+    it { assert !valid2?(111123) }
+    it { assert !valid2?(111111) }
+    it { assert !valid2?(223450) }
+    it { assert !valid2?(123789) }
+    it { assert !valid2?(123444) }
+    it { assert valid2?(111122) }
   end
 
   it 'part 1' do
@@ -49,6 +50,6 @@ describe 'password' do
     input = (273025..767253)
     assert_equal 273025, input.first
     assert_equal 767253, input.last
-    assert_equal 598, input.count { |num| valid?(num) && two_adjacent_matching?(num) }
+    assert_equal 598, input.count { |num| valid2?(num) }
   end
 end
