210b3e3670890e5ea10b2eff40b5086f0a8ae0f1
Dave Goddard <dave@goddard.id.au>
2022-12-03 19:12:38 +1100

    day3

--- 2022/src/day3.rs
+++ 2022/src/day3.rs
@@ -1,29 +1,38 @@
 #[aoc_generator(day3)]
-pub fn input_generator(input: &str) -> Vec<(String, String)> {
-    input.lines()
-        .map(|l| {
-            let half = l.len() / 2;
-            return (l[..half].to_string(), l[half..].to_string());
-        })
-        .collect()
+pub fn input_generator(input: &str) -> Vec<String> {
+    input.lines().map(|l| String::from(l)).collect()
+}
+
+fn priority(c: char) -> u32 {
+        let mut num = c as u32;
+        match num {
+            (65..=90) => num - 64 + 26,
+            (97..=122) => num - 96,
+            _ => unreachable!(),
+        }
 }
 
 #[aoc(day3, part1)]
-pub fn solve_part1(input: &[(String, String)]) -> u32 {
-    input.iter().map(|(l, r)| {
+pub fn solve_part1(input: &[String]) -> u32 {
+    input.iter().map(|line| {
+        let half = line.len() / 2;
+        let l = line[..half].to_string();
+        let r = line[half..].to_string();
         let inboth = l.chars().find(|c| r.contains(*c)).unwrap();
-        let mut num = inboth as u32;
-        match num {
-            (65...90) => num - 64 + 26,
-            (97...122) => num - 96,
-            _ => unreachable!(),
-        }
+        priority(inboth)
     }).sum()
 }
 
 #[aoc(day3, part2)]
-pub fn solve_part2(input: &[(String, String)]) -> u32 {
-    0
+pub fn solve_part2(input: &[String]) -> u32 {
+    input.chunks(3).map(|group| {
+        // println!("GROUP: {:?}", group);
+        let x = &group[0];
+        let y = &group[1];
+        let z = &group[2];
+        let shared = x.chars().find(|c| y.contains(*c) && z.contains(*c)).unwrap();
+        priority(shared)
+    }).sum()
 }
 
 #[cfg(test)]
@@ -32,12 +41,6 @@ mod tests {
 
     const SAMPLE: &str = "vJrwpWtwJgWrhcsFMMfFFhFp\njqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\nPmmdzqPrVvPwwTWBwg\nwMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\nttgJtRGJQctTZtZT\nCrZsJsPPZsGzwwsLwLmpwMDw";
     const SAMPLE1: &str = "vJrwpWtwJgWrhcsFMMfFFhFp";
-
-    #[test]
-    fn sample1() {
-        assert_eq!(gen("abcd\nefgh\n"), vec![(String::from("ab"), String::from("cd")), (String::from("ef"), String::from("gh"))]);
-        assert_eq!(gen(SAMPLE1), vec![(String::from("vJrwpWtwJgWr"), String::from("hcsFMMfFFhFp"))]);
-    }
 
     #[test]
     fn sample2() {
@@ -55,5 +58,11 @@ mod tests {
     fn sample4() {
         let data = gen(SAMPLE);
         assert_eq!(part1(&data), 157);
+    }
+
+    #[test]
+    fn sample5() {
+        let data = gen(SAMPLE);
+        assert_eq!(part2(&data), 70);
     }
 }
