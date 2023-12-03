93cf5e4f5d282e39fc7c9fc434ce122b4d849c9e
Dave Goddard <dave@goddard.id.au>
2023-12-03 18:19:43 +1100

    day 1 part 2

--- 2023/day1.rb
+++ 2023/day1.rb
@@ -1,19 +1,55 @@
 require 'minitest/autorun'
 
+NUMBERS = %w{one two three four five six seven eight nine}.freeze
+
 class Day1 < Minitest::Test
-  def day1(filename)
+  def part1(filename)
     txt = File.read(filename)
-    lines = txt.split("\n").map { |l| l.split('').grep(/\d/) }
-    actual = lines.map { |l| (l.first + l.last).to_i }.sum
+    lines = txt.split("\n")
+    lines = lines.map { _1.split('').grep(/\d/) }
+    lines.map { (_1.first + _1.last).to_i }.sum
   end
 
   def test_sample1
-    num = day1('fixtures/day1/sample1.txt')
+    num = part1('fixtures/day1/sample1.txt')
     assert_equal num, 142
   end
 
-  def test_day1
-    num = day1('fixtures/day1/input1.txt')
+  def test_part1
+    num = part1('fixtures/day1/input1.txt')
     assert_equal num, 55607
+  end
+
+  def part2(filename)
+    txt = File.read(filename)
+    lines = txt.split("\n").map do |l|
+      arr = []
+      while l.length > 0
+        if l.match(/^\d/)
+          arr << l[0].to_i
+        else
+          NUMBERS.each.with_index do |w,idx|
+            if l.start_with?(w)
+              arr << idx + 1
+              l = l[(w.length - 1)..]
+              break
+            end
+          end
+        end
+        l = l[1..]
+      end
+      arr
+    end
+    lines.map { (_1.first * 10) + _1.last }.sum
+  end
+
+  def test_sample2
+    num = part2('fixtures/day1/sample2.txt')
+    assert_equal num, 281
+  end
+
+  def test_part2
+    num = part2('fixtures/day1/input1.txt')
+    assert_equal num, 55309
   end
 end
