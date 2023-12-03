456beae235c364298d7155f44372eb32162b75c1
Dave Goddard <dave@goddard.id.au>
2023-12-03 20:25:32 +1100

    day2 - part2

--- 2023/day2.rb
+++ 2023/day2.rb
@@ -31,4 +31,33 @@ class Day2 < Minitest::Test
     num = part1('fixtures/day2/input.txt')
     assert_equal num, 2771
   end
+
+  def part2(filename)
+    lines = File.read(filename).split("\n")
+    games = lines.map do |l|
+      game_num = l.match(/^Game (\d+): /)[1].to_i
+      games = l.gsub!(/^Game (\d+): /, '').split(/;\s*/).map { _1.split(/,\s*/) }
+      games = games.map { |g| g.map { m = _1.match(/^(\d+) (red|green|blue)/); [m[2], m[1].to_i] } }
+      [game_num, games]
+    end
+    powers = games.map do |(_, gs)|
+      mins = {}
+      gs.each { |g| g.each { |(c, v)| mins[c] ||= [] ; mins[c] << v } }
+      mins
+    end
+    # puts powers.map(&:inspect).join("\n")
+    powers = powers.map { |ps| ps.map { |_k, v| v.max }.reduce(:*) }
+    # puts powers.map(&:inspect).join("\n")
+    powers.sum
+  end
+
+  def test_sample2
+    num = part2('fixtures/day2/sample1.txt')
+    assert_equal num, 2286
+  end
+
+  def test_part2
+    num = part2('fixtures/day2/input.txt')
+    assert_equal num, 70924
+  end
 end
