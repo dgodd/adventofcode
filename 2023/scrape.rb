ac44083783fff67141fac701bd4e52f355041db3
Dave Goddard <dave@goddard.id.au>
2023-12-03 21:44:09 +1100

    use aoc to scrape

--- 2023/scrape.rb
+++ 2023/scrape.rb
@@ -4,22 +4,25 @@ require 'nokogiri'
 require 'excon'
 require 'reverse_markdown'
 
+
 AOC_SESSION = File.read('/Users/dgodd/.adventofcode.session').chomp
-
-# res = Excon.get('https://adventofcode.com/2023', headers: { Cookie: "session=#{AOC_SESSION}" })
-# doc = Nokogiri::HTML(res.body)
-# days = doc.css('pre.calendar a').map { |a| a[:href] }
-
-day = '4'
-res = Excon.get("https://adventofcode.com/2023/day/#{day}", headers: { Cookie: "session=#{AOC_SESSION}" })
+res = Excon.get('https://adventofcode.com/2023', headers: { Cookie: "session=#{AOC_SESSION}" })
 doc = Nokogiri::HTML(res.body)
-FileUtils.mkdir_p("fixtures/day#{day}")
-txt = ReverseMarkdown.convert(doc.css('article.day-desc').to_s)
-puts txt
-File.write("fixtures/day#{day}/README.txt", txt)
-File.write("fixtures/day#{day}/sample1.txt", doc.css('article.day-desc > pre > code').text)
+days = doc.css('pre.calendar a').map { |a| a[:href] }
 
-res = Excon.get("https://adventofcode.com/2023/day/#{day}/input", headers: { Cookie: "session=#{AOC_SESSION}" })
-File.write("fixtures/day#{day}/input.txt", res.body)
+day = days.first.match(/(\d+)$/)[1].to_i
+system("aoc", "download", "--day=#{day}", "--input-file=fixtures/day#{day}/input.txt", "--puzzle-file=fixtures/day#{day}/README.txt", "--overwrite")
+
+# day = '4'
+# res = Excon.get("https://adventofcode.com/2023/day/#{day}", headers: { Cookie: "session=#{AOC_SESSION}" })
+# doc = Nokogiri::HTML(res.body)
+# FileUtils.mkdir_p("fixtures/day#{day}")
+# txt = ReverseMarkdown.convert(doc.css('article.day-desc').to_s)
+# puts txt
+# File.write("fixtures/day#{day}/README.txt", txt)
+# File.write("fixtures/day#{day}/sample1.txt", doc.css('article.day-desc > pre > code').text)
+# 
+# res = Excon.get("https://adventofcode.com/2023/day/#{day}/input", headers: { Cookie: "session=#{AOC_SESSION}" })
+# File.write("fixtures/day#{day}/input.txt", res.body)
 
 system('git', 'add', "fixtures/day#{day}")
