b041cd98a58aa617f9289f2275bcbc06d1f4dc64
Dave Goddard <dave@goddard.id.au>
2023-12-08 20:40:31 +1100

    day8 - part2

---
+++ 2023/day8.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require 'minitest/focus'
+
+class Day8 < Minitest::Test
+  def parse(filename)
+    lines = File.read(filename).split("\n")
+    sequence = lines.shift.split('').map { |m| m == 'L' ? 0 : 1 }
+    lines.shift
+    data = {}
+    lines.each do |l|
+      m = l.match(/(.*)\s+=\s+\((.*),\s+(.*)\)/)
+      data[m[1]] = [m[2], m[3]]
+    end
+    [sequence, data]
+  end
+
+  def part1(filename)
+    (seq, data) = parse(filename)
+    pos = 'AAA'
+    (0..).each do |steps|
+      instr = seq[steps % seq.length]
+      pos = data[pos][instr] 
+      return steps + 1 if pos == 'ZZZ'
+    end
+  end
+
+  def test_sample1a
+    num = part1('fixtures/day8/sample_a.txt')
+    assert_equal 2, num
+  end
+
+  def test_sample1b
+    num = part1('fixtures/day8/sample_b.txt')
+    assert_equal 6, num
+  end
+
+  def test_part1
+    num = part1('fixtures/day8/input.txt')
+    assert_equal 20221, num
+  end
+end
