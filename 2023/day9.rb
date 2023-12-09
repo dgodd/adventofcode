d1a246f218044153f54b017b5f0f76c16ca1ef2d
Dave Goddard <dave@goddard.id.au>
2023-12-10 10:39:05 +1100

    day9 - part2

--- 2023/day9.rb
+++ 2023/day9.rb
@@ -39,8 +39,38 @@ class Day9 < Minitest::Test
     end
   end
 
-  focus def test_part1
+  def test_part1
     num = part1('fixtures/day9/input.txt')
     assert_equal 1743490457, num
+  end
+
+  def prev_val(values)
+    # puts "RCVD: #{values.inspect}"
+    return 0 if values.all?(0)
+
+    diffs = values.each_cons(2).map { |(a, b)| b - a }
+
+    # p diffs
+    pv = prev_val(diffs)
+    # puts "PV: #{pv}"
+
+    # puts "PV2: #{values.first - pv} -- #{values.first} + #{pv}"
+    values.first - pv
+  end
+
+  def test_sample_1_rev
+    assert_equal 5, prev_val(%w{10  13  16  21  30  45}.map(&:to_i))
+  end
+
+  def part2(filename)
+    values = parse(filename)
+    values.sum do |vs|
+      prev_val(vs)
+    end
+  end
+
+  def test_part2
+    num = part2('fixtures/day9/input.txt')
+    assert_equal 1053, num
   end
 end
