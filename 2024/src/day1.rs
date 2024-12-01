d1e806ab5c289ec42b5d94f1c2ef4055f5345f6e
Dave Goddard <dave@goddard.id.au>
2024-12-01 16:41:24 +1100

    20204 day1 part2 - refactor for input_generator

--- 2024/src/day1.rs
+++ 2024/src/day1.rs
@@ -1,48 +1,38 @@
-#[aoc(day1, part1)]
-pub fn part1(input: &str) -> i64 {
+use indoc::indoc;
+
+#[aoc_generator(day1)]
+pub fn input_generator(input: &str) -> (Vec<i64>, Vec<i64>) {
     let mut list1 = Vec::new();
     let mut list2 = Vec::new();
     for line in input.lines() {
-        // println!("LINE {:?}", line);
         let a: Vec<i64> = line
             .split_whitespace()
             .map(|i| i.parse().unwrap())
             .collect();
-        // println!("A: {:?}", a);
         list1.push(a[0]);
         list2.push(a[1]);
     }
-    println!("LIST 1 {:?}", list1);
-    println!("LIST 2 {:?}", list2);
+    (list1, list2)
+}
+
+#[aoc(day1, part1)]
+pub fn part1(input: &(Vec<i64>, Vec<i64>)) -> i64 {
+    let (ref mut list1, ref mut list2) = input.clone();
 
     list1.sort();
     list2.sort();
-    println!("LIST 1 {:?}", list1);
-    println!("LIST 2 {:?}", list2);
-
     list1
         .iter()
         .zip(list2)
-        .fold(0, |sum, (a, b)| sum + (a - b).abs())
+        .fold(0, |sum, (a, b)| sum + (*a - *b).abs())
 }
 
 #[aoc(day1, part2)]
-pub fn part2(input: &str) -> u64 {
-    let mut list1 = Vec::new();
-    let mut list2 = Vec::new();
-    for line in input.lines() {
-        // println!("LINE {:?}", line);
-        let a: Vec<i64> = line
-            .split_whitespace()
-            .map(|i| i.parse().unwrap())
-            .collect();
-        // println!("A: {:?}", a);
-        list1.push(a[0]);
-        list2.push(a[1]);
-    }
+pub fn part2(input: &(Vec<i64>, Vec<i64>)) -> i64 {
+    let (list1, list2) = input;
 
     list1.iter().fold(0, |sum, a| {
-        sum + ((*a as u64) * list2.iter().filter(|b| &a == b).count() as u64)
+        sum + ((*a as i64) * list2.iter().filter(|b| &a == b).count() as i64)
     })
 }
 
@@ -52,7 +42,15 @@ mod tests {
 
     #[test]
     fn find_numbers_example() {
-        let input = "3   4\n4   3\n2   5\n1   3\n3   9\n3   3";
-        assert_eq!(part2(input), 31);
+        let input = indoc! {"
+            3   4
+            4   3
+            2   5
+            1   3
+            3   9
+            3   3
+        "};
+        println!("{}", input);
+        assert_eq!(part2(&input_generator(input)), 31);
     }
 }
