02b1a28c4ded12c6b98ba79489e669ca346c2b41
Dave Goddard <dave@goddard.id.au>
2024-12-03 19:19:21 +1100

    2024 day3 part1 loop

--- 2024/src/day3.rs
+++ 2024/src/day3.rs
@@ -1,6 +1,6 @@
 use regex::Regex;
 
-#[aoc(day3, part1)]
+#[aoc(day3, part1, regex)]
 pub fn part1(input: &str) -> i64 {
     let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
     let caps: Vec<_> = re
@@ -8,6 +8,41 @@ pub fn part1(input: &str) -> i64 {
         .map(|r| (r[1].parse::<i64>().unwrap(), r[2].parse::<i64>().unwrap()))
         .collect();
     caps.iter().fold(0, |sum, (a, b)| sum + (a * b))
+}
+
+#[aoc(day3, part1, loop)]
+pub fn part1_loop(input: &str) -> i64 {
+    let mut state = 0;
+    let mut sum: i64 = 0;
+    let mut a: i64 = 0;
+    let mut b: i64 = 0;
+    for x in input.chars() {
+        if x == 'm' {
+            state = 1;
+        } else if state == 1 && x == 'u' {
+            state = 2;
+        } else if state == 2 && x == 'l' {
+            state = 3;
+        } else if state == 3 && x == '(' {
+            state = 4;
+        } else if state == 4 && x >= '0' && x <= '9' {
+            a = (a * 10) + (x as usize - '0' as usize) as i64;
+        } else if state == 4 && x == ',' {
+            state = 5;
+        } else if state == 5 && x >= '0' && x <= '9' {
+            b = (b * 10) + (x as usize - '0' as usize) as i64;
+        } else if state == 5 && x == ')' {
+            sum += a * b;
+            state = 0;
+            a = 0;
+            b = 0;
+        } else {
+            state = 0;
+            a = 0;
+            b = 0;
+        }
+    }
+    sum
 }
 
 #[aoc(day3, part2)]
