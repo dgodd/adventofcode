8c98a1b7ffcc041e562dbc6f475edb0e8d5543ba
Dave Goddard <dave@goddard.id.au>
2024-12-03 19:02:20 +1100

    2024 day3
    AOC 2024
    Day 3 - Part 1 : 192767529
            generator: 29.291µs,
            runner: 2.022667ms
    
    Day 3 - Part 2 : 104083373
            generator: 125ns,
            runner: 990.875µs

--- 2024/src/day3.rs
+++ 2024/src/day3.rs
@@ -3,8 +3,30 @@ use regex::Regex;
 #[aoc(day3, part1)]
 pub fn part1(input: &str) -> i64 {
     let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
-    let caps: Vec<_> = re.captures_iter(input).map(|r| (r[1].parse::<i64>().unwrap(), r[2].parse::<i64>().unwrap())).collect();
-    caps.iter().fold(0, |sum, (a,b)| sum + (a * b))
+    let caps: Vec<_> = re
+        .captures_iter(input)
+        .map(|r| (r[1].parse::<i64>().unwrap(), r[2].parse::<i64>().unwrap()))
+        .collect();
+    caps.iter().fold(0, |sum, (a, b)| sum + (a * b))
+}
+
+#[aoc(day3, part2)]
+pub fn part2(input: &str) -> i64 {
+    let re = Regex::new(r"^mul\((\d{1,3}),(\d{1,3})\)").unwrap();
+    let mut enabled = true;
+    let mut sum = 0;
+    for i in 0..input.len() {
+        let curr = &input[i..];
+        if curr.starts_with("do()") {
+            enabled = true;
+        } else if curr.starts_with("don't()") {
+            enabled = false;
+        } else if enabled && re.is_match(curr) {
+            let r = re.captures(curr).unwrap();
+            sum += r[1].parse::<i64>().unwrap() * r[2].parse::<i64>().unwrap();
+        }
+    }
+    sum
 }
 
 #[cfg(test)]
@@ -12,8 +34,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn find_numbers_example() {
+    fn test_part1() {
         let input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
         assert_eq!(part1(&input), 161);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+        assert_eq!(part2(&input), 48);
     }
 }
